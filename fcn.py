e8b0ba5a024e9e31ab4dd2ffa25ff29f8e97ba34
Danqi Liao <danqiliao@gmail.com>
2021-05-01 13:57:12 -0700

    modify decoder architecture

--- fcn.py
+++ fcn.py
@@ -156,20 +156,35 @@ class FCN32s(nn.Module):
         self.relu = nn.ReLU(inplace=True)
 
         # Decoder
+        # self.deconv1 = nn.ConvTranspose2d(
+        #     512, 512, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        # self.bn1 = nn.BatchNorm2d(512)
+        # self.deconv2 = nn.ConvTranspose2d(
+        #     512, 256, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        # self.bn2 = nn.BatchNorm2d(256)
+        # self.deconv3 = nn.ConvTranspose2d(
+        #     256, 128, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        # self.bn3 = nn.BatchNorm2d(128)
+        # self.deconv4 = nn.ConvTranspose2d(
+        #     128, 64, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        # self.bn4 = nn.BatchNorm2d(64)
+        # self.deconv5 = nn.ConvTranspose2d(
+        #     64, 32, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        # self.bn5 = nn.BatchNorm2d(32)
+        # self.classifier = nn.Conv2d(32, n_class, kernel_size=1)
         self.deconv1 = nn.ConvTranspose2d(
-            512, 512, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+            512, 512, kernel_size=4, stride=2, padding=1)
         self.bn1 = nn.BatchNorm2d(512)
         self.deconv2 = nn.ConvTranspose2d(
-            512, 256, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+            512, 256, kernel_size=4, stride=2, padding=1)
         self.bn2 = nn.BatchNorm2d(256)
         self.deconv3 = nn.ConvTranspose2d(
-            256, 128, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+            256, 128, kernel_size=4, stride=2, padding=1)
         self.bn3 = nn.BatchNorm2d(128)
         self.deconv4 = nn.ConvTranspose2d(
-            128, 64, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+            128, 64, kernel_size=4, stride=2, padding=1)
         self.bn4 = nn.BatchNorm2d(64)
-        self.deconv5 = nn.ConvTranspose2d(
-            64, 32, kernel_size=3, stride=2, padding=1, dilation=1, output_padding=1)
+        self.deconv5 = nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1)
         self.bn5 = nn.BatchNorm2d(32)
         self.classifier = nn.Conv2d(32, n_class, kernel_size=1)
 
@@ -195,12 +210,16 @@ class FCN32s(nn.Module):
 
         # size=(N, 512, x.H/16, x.W/16)
         score = self.bn1(self.relu(self.deconv1(x5)))
+        #print('score.shape: ', score.shape)
         # size=(N, 256, x.H/8, x.W/8)
         score = self.bn2(self.relu(self.deconv2(score)))
+        #print('score.shape: ', score.shape)
         # size=(N, 128, x.H/4, x.W/4)
         score = self.bn3(self.relu(self.deconv3(score)))
+        #print('score.shape: ', score.shape)
         # size=(N, 64, x.H/2, x.W/2)
         score = self.bn4(self.relu(self.deconv4(score)))
+        #print('score.shape: ', score.shape)
         score = self.bn5(self.relu(self.deconv5(score)))  # size=(N, 32, x.H, x.W)
         
         # size=(N, n_class, x.H/1, x.W/1)
